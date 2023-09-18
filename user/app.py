15d6ccc3e6565cce255b8898449de0b1d3b03d4d
dima999572 <dima_dubinevich@mail.ru>
2023-09-18 17:59:05 +0200

    Create initial user microservice structure

---
+++ user/app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from datetime import timedelta
+
+import models 
+from routes import user_blueprint
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dima999572'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+# TODO: Find a good way of reference to db using relative path, not absolute path (earlier relative path did not work)
+import os
+db_relative_path = os.path.join(os.getcwd(), 'database', 'user.db')
+print(db_relative_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_relative_path}'
+models.init_app(app)
+app.register_blueprint(user_blueprint)
+
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.get(user_id)
+
+
+migrate = Migrate(app, models.db)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)   
---
+++ user/routes.py
@@ -0,0 +1,104 @@
+from flask import Blueprint, jsonify, request, make_response
+from flask_login import current_user, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from models import db, User
+
+
+user_blueprint = Blueprint('user_api_routes', __name__, url_prefix='/api/user/')
+
+
+@user_blueprint.route('/all', methods=['GET'])
+def get_all_users():
+    all_users = User.query.all()
+    result = [user.serialize() for user in all_users]
+    return make_response(jsonify({
+        'message': 'Returning all users',
+        'response': result
+    }), 200)
+
+
+@user_blueprint.route('/create', methods=['POST'])
+def create_user():
+    try:
+        user = User()
+        user.username = request.form["username"]
+        user.password = generate_password_hash(request.form['password'], method='sha256')
+        user.is_admin = True
+        db.session.add(user)
+        db.session.commit()
+
+        response = {
+            'message': 'User Created', 
+            'result': user.serialize() 
+        }
+
+    except Exception as e:
+        print(e)
+        response = {
+            'message': 'Error in creating response'
+        }
+
+    return jsonify(response)
+
+
+@user_blueprint.route('login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return make_response(jsonify({
+            'message': 'Wrong username or password. Please check your credentials and try again.'
+        }), 401)
+    if check_password_hash(user.password, password):
+        user.update_api_key()
+        db.session.commit()
+        login_user(user)
+        return make_response(jsonify({
+            'message': 'Logged in',
+            'api_key': user.api_key
+        }), 200)
+    
+    return make_response(jsonify({
+        'message': 'Wrong username or password. Please check your credentials and try again.'
+    }), 401)
+
+
+@user_blueprint.route("/logout", methods=['POST'])
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
+        return make_response(jsonify({
+            'message': 'Logged out'
+        }), 200)
+    else:
+        return make_response(jsonify({
+            'message': 'No user logged in'
+        }), 401)
+
+
+@user_blueprint.route('/<username>/exist', methods=['GET'])
+def user_exist(username):
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return make_response(jsonify({
+            'result': True
+        }), 200)
+    else: 
+        return make_response(jsonify({
+            'result': False
+        }), 404)
+
+
+@user_blueprint.route('/', methods=['GET'])
+def get_current_user():
+    if current_user.is_authenticated:
+        return make_response(jsonify({
+            'result': current_user.serialize()
+        }), 200)
+    else:
+        return make_response(jsonify({
+            'message': 'User not logged in'
+        }), 401)
